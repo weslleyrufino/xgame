9010e00fd08f61a43ebcc1c72a42898ffc348e3f
Weslley Lima <weslley.lima@centauro.com.br>
2020-05-20 21:58:56 -0300

    Commit

---
+++ XGame.Domain/Interfaces/Services/Base/IServiceBase.cs
@@ -0,0 +1,10 @@
+﻿using prmToolkit.NotificationPattern;
+using System;
+
+namespace XGame.Domain.Interfaces.Services.Base
+{
+    public interface IServiceBase : INotifiable, IDisposable
+    {
+        // Estou usando o "Notifications" no ControllerBase, por isso preciso do INotifiable aqui.
+    }
+}
